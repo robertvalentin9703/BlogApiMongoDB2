21c2c71274824b8c7c7a9ea022b8a1e9734f062e
Robert Valentin <robert.carp@crystal-system.eu>
2021-10-14 15:04:00 +0300

    App.prop problem

---
+++ src/main/java/eu/crystal/BlogApiMongoDB/BlogApiMongoDB2Application.java
@@ -0,0 +1,13 @@
+package eu.crystal.BlogApiMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogApiMongoDB2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApiMongoDB2Application.class, args);
+	}
+
+}
